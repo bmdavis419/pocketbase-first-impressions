fdac295037a5c1fadde6a3b78347eab83587bfe9
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-01-13 14:43:46 -0500

    init project

---
+++ main.go
@@ -0,0 +1,16 @@
+// main.go
+package main
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
